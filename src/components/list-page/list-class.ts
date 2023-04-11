17c45d29737b4616cc3d58a60fba03cff594668a
aotopkov <aotopkov@gmail.com>
2023-04-11 15:47:49 +0300

    add: list page and class

---
+++ src/components/list-page/list-class.ts
@@ -0,0 +1,143 @@
+export class Node<T> {
+  value: T;
+  next: Node<T> | null;
+  constructor(value: T, next?: Node<T> | null) {
+    this.value = value;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+interface ILinkedList<T> {
+  append: (element: T) => void;
+  insertAt: (element: T, position: number) => void;
+  getSize: () => number;
+}
+
+class LinkedList<T> implements ILinkedList<T> {
+  private head: Node<T> | null;
+  private size: number;
+  private container: T[];
+  constructor() {
+    this.head = null;
+    this.size = 0;
+    this.container = [];
+  }
+
+  insertAt(element: T, index: number) {
+    if (index < 0 || index > this.size) {
+      console.log("Enter a valid index");
+      return;
+    } else {
+      const node = new Node(element);
+
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let curr = this.head;
+        let prev = null;
+        let currIndex = 0;
+
+        while (currIndex < index && curr != null) {
+          prev = curr;
+          curr = curr.next;
+          currIndex++;
+        }
+        if (prev != null) {
+          prev.next = node;
+          node.next = curr;
+        }
+      }
+
+      this.size++;
+    }
+  }
+
+  deleteAt(index: number) {
+    if (index < 0 || index > this.size) {
+      console.log("Enter a valid index");
+      return;
+    } else {
+      let curr = this.head;
+      let prev = null;
+      let currIndex = 0;
+      if (index - 1 === 0 && curr !== null && curr.next === null) {
+        return (this.head = null);
+      } else {
+        while (currIndex < index - 1 && curr != null) {
+          prev = curr;
+          curr = curr.next;
+          currIndex++;
+        }
+        if (prev != null && curr != null) {
+          prev.next = curr.next;
+        }
+      }
+
+      this.size--;
+    }
+  }
+
+  append(element: T) {
+    const node = new Node(element);
+    let current;
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  prepend(element: T) {
+    const node = new Node(element);
+    node.next = this.head;
+    this.head = node;
+    this.size++;
+  }
+
+  deleteHead() {
+    if (this.head !== null) {
+      if (this.head.next !== null) {
+        let current = this.head.next;
+        this.head = current;
+      } else {
+        this.head = null;
+      }
+    }
+  }
+
+  deleteTail() {
+    if (this.head !== null) {
+      let curr = this.head;
+      let prev = curr;
+      while (curr.next) {
+        prev = curr;
+        curr = curr.next;
+      }
+      prev.next = null;
+    }
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  toArray() {
+    let curr = this.head;
+    this.container = [];
+    while (curr) {
+      this.container.push(curr.value);
+      curr = curr.next;
+    }
+    return this.container;
+  }
+}
+
+export const list = new LinkedList<string>();
--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,51 +1,232 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import styles from "./list.module.css";
 import { Circle } from "../ui/circle/circle";
-
-interface Node {
-  value: string;
-  next: Node | null;
-}
+import { delay, useInput } from "../../utils/utils";
+import { list } from "./list-class";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
+import { ElementStates } from "../../types/element-states";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const ListPage: React.FC = () => {
-  const [head, setHead] = useState<Node>({
-    value: '0',
-    next: { value: '34', next: { value: '8', next: { value: '0', next: null } } },
+  const { input, changeInput, setValue } = useInput({ value: "", index: 0 });
+  const [listState, setListState] = useState<string[]>([]);
+  const [status, setStatus] = useState({
+    append: false,
+    prepend: false,
+    deleteHead: false,
+    deleteTail: false,
+    insertAt: false,
+    deleteAt: false,
+    done: false,
+    value: "",
+    index: NaN,
   });
 
-  let curr = head
+  function getRandomList() {
+    let length = Math.floor(Math.random() * (6 - 2) + 2);
+    for (let i = 1; i < length; i++) {
+      list.append(String(Math.floor(Math.random() * 100)));
+    }
+    setListState(list.toArray());
+  }
+
+  useEffect(() => {
+    getRandomList();
+  }, []);
+
+  async function insertNode(method: "append" | "prepend") {
+    setStatus({
+      ...status,
+      prepend: method === "prepend" ? true : false,
+      append: method === "append" ? true : false,
+      value: input.value,
+    });
+    await delay(SHORT_DELAY_IN_MS);
+    method === "prepend" ? list.prepend(input.value) : list.append(input.value);
+    setListState(list.toArray());
+    setValue({ value: "" });
+    setStatus((status) => ({
+      ...status,
+      value: "",
+      done: true,
+    }));
+    await delay(SHORT_DELAY_IN_MS);
+    setStatus({
+      ...status,
+      append: false,
+      prepend: false,
+      done: false,
+    });
+  }
+
+  async function deleteNode(method: "head" | "tail") {
+    setStatus({
+      ...status,
+      deleteHead: method === "head" ? true : false,
+      deleteTail: method === "tail" ? true : false,
+      value: method === "head" ? listState[0] : listState[listState.length - 1],
+    });
+    await delay(SHORT_DELAY_IN_MS);
+    method === "head" ? list.deleteHead() : list.deleteTail();
+    setListState(list.toArray());
+    setStatus({ ...status, deleteHead: false, deleteTail: false, value: "" });
+  }
+
+  async function fromIndex(method: "insert" | "delete") {
+    for (let i = 0; i < input.index; i++) {
+      setStatus({
+        ...status,
+        index: i,
+        insertAt: method === "insert" ? true : false,
+        deleteAt: method === "delete" ? true : false,
+        value: method === "insert" ? input.value : listState[i],
+      });
+      await delay(SHORT_DELAY_IN_MS);
+    }
+    method === "insert"
+      ? list.insertAt(input.value, input.index)
+      : list.deleteAt(input.index);
+    setListState(list.toArray());
+    setStatus((status) => ({
+      ...status,
+      insertAt: false,
+      deleteAt: false,
+      done: true,
+    }));
+    await delay(SHORT_DELAY_IN_MS);
+    setStatus({ ...status, done: false, index: NaN });
+    setValue({ value: "", index: 0 });
+  }
 
   return (
     <SolutionLayout title="Связный список">
-    {/* //   <div className={styles.input_container}>
-    //     <div className={styles.input}>
-    //       <Input */}
-    {/* //         maxLength={4}
-    //         isLimitText={true}
-    //         placeholder="Введите значение"
-    //       ></Input>
-    //     </div> */}
-    {/* //     <Button text="Добавить в head" linkedList={"small"}></Button>
-    //     <Button text="Добавить в tail" linkedList={"small"}></Button>
-    //     <Button text="Удалить из head" linkedList={"small"}></Button>
-    //     <Button text="Удалить из tail" linkedList={"small"}></Button>
-    //   </div> */}
-    {/* //   <div className={styles.input_container}>
-    //     <div className={styles.input}>
-    //       <Input placeholder="Введите индекс"></Input>
-    //     </div>
-    //     <Button linkedList={"big"} text="Добавить по индексу"></Button>
-    //     <Button linkedList={"big"} text="Удалить по индексу"></Button>
-    //   </div>
-    //   <div>
-
-    //     { while(curr) { */}
-    {/* //       return (<><Circle letter={curr.value}></Circle></>)}
-    //     }
-    //   </div> */}
+      <div className={styles.input_container}>
+        <div className={styles.input}>
+          <Input
+            name="value"
+            maxLength={4}
+            isLimitText={true}
+            onChange={changeInput}
+            placeholder="Введите значение"
+            value={input.value}
+          ></Input>
+        </div>
+        <Button
+          text="Добавить в head"
+          linkedList={"small"}
+          onClick={() => insertNode("prepend")}
+          isLoader={status.prepend}
+          disabled={input.value === "" ? true : false}
+        ></Button>
+        <Button
+          text="Добавить в tail"
+          linkedList={"small"}
+          onClick={() => insertNode("append")}
+          isLoader={status.append}
+          disabled={input.value === "" ? true : false}
+        ></Button>
+        <Button
+          text="Удалить из head"
+          linkedList={"small"}
+          onClick={() => deleteNode("head")}
+          isLoader={status.deleteHead}
+          disabled={!listState.length}
+        ></Button>
+        <Button
+          text="Удалить из tail"
+          linkedList={"small"}
+          onClick={() => deleteNode("tail")}
+          isLoader={status.deleteTail}
+          disabled={listState.length === 1 || !listState.length}
+        ></Button>
+      </div>
+      <div className={styles.input_container}>
+        <div className={styles.input}>
+          <Input
+            type="number"
+            placeholder="Введите индекс"
+            name="index"
+            value={input.index}
+            onChange={changeInput}
+            max={listState.length}
+          ></Input>
+        </div>
+        <Button
+          linkedList={"big"}
+          text="Добавить по индексу"
+          onClick={() => fromIndex("insert")}
+          disabled={input.index < 1 || input.value === "" ? true : false}
+        ></Button>
+        <Button
+          onClick={() => fromIndex("delete")}
+          linkedList={"big"}
+          text="Удалить по индексу"
+          disabled={input.index < 1 ? true : false}
+        ></Button>
+      </div>
+      <ul className={styles.list}>
+        {listState.map((elem, index) => {
+          let value = String(elem);
+          return (
+            <li key={index} className={styles.list_item}>
+              <Circle
+                letter={
+                  (index === 0 && status.deleteHead) ||
+                  (index === listState.length - 1 && status.deleteTail)
+                    ? ""
+                    : value
+                }
+                head={
+                  (index === 0 && status.prepend && !status.done) ||
+                  (index === listState.length - 1 &&
+                    status.append &&
+                    !status.done) ||
+                  (index === status.index && status.insertAt) ? (
+                    <Circle
+                      letter={status.value}
+                      isSmall={true}
+                      state={ElementStates.Changing}
+                    ></Circle>
+                  ) : (
+                    "" || (index === 0 ? "head" : "")
+                  )
+                }
+                tail={
+                  (index === 0 && status.deleteHead) ||
+                  (index === listState.length - 1 && status.deleteTail) ||
+                  (index === status.index && status.deleteAt) ? (
+                    <Circle
+                      letter={status.value}
+                      isSmall={true}
+                      state={ElementStates.Changing}
+                    ></Circle>
+                  ) : (
+                    "" ||
+                    (index === listState.length - 1 && listState.length !== 1
+                      ? "tail"
+                      : "")
+                  )
+                }
+                state={
+                  status.done
+                    ? (index === 0 && status.prepend) ||
+                      index === status.index + 1 ||
+                      (index === listState.length - 1 && status.append)
+                      ? ElementStates.Modified
+                      : ElementStates.Default
+                    : index < status.index
+                    ? ElementStates.Changing
+                    : undefined
+                }
+              ></Circle>
+              {index !== listState.length - 1 && <ArrowIcon></ArrowIcon>}
+            </li>
+          );
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
